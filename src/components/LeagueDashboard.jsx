3e3dada7cb83bd1b42cbfe62a57653206b3803e8
phillipsha10 <phillipsha10@gmail.com>
2026-08-22 19:23:27 -0400

    Update LeagueDashboard.jsx

--- src/components/LeagueDashboard.jsx
+++ src/components/LeagueDashboard.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import StandingsTable from './StandingsTable';
-import ChampionshipHistory from './ChampionshipHistory';
+import ChampionshipHistory from './ChampionshipHistory_2';
 
 const LeagueDashboard = ({ leagueData, darkMode }) => {
   const [activeTab, setActiveTab] = useState('standings');
